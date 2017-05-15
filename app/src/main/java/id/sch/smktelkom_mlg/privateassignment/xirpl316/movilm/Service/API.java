b201b80443db85377d56dd0ccc7f21a79d2a63cb
leovano99 <aleovano@gmail.com>
2017-05-15 21:52:51 +0700

    Rest Commit

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl316/movilm/Service/API.java
@@ -0,0 +1,6 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl316.movilm.Service;
+
+public class API {
+    static public String API_URL = "http://api.themoviedb.org/3/discover/movie";
+    static public String API_KEY = "a867b7da110c643955a12e3c96785e6c";
+}
